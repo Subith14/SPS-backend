109c13899a862c271b0a5de5192a255897057e03
Subith14 <subithsoosadimai@gmail.com>
2024-10-05 16:24:09 +0530

    ok code

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
-// db.js
+// Import required modules
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // Load environment variables from .env
 
-// Initialize Sequelize with DATABASE_URL from environment variables
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// Initialize Sequelize with credentials
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'spsdb', 'postgres', 'Subith123', {
+  host: process.env.DB_HOST || 'database-1.c7a682s8k7u3.ap-southeast-2.rds.amazonaws.com',
   dialect: 'postgres',
-  logging: false, // Disable logging for cleaner console output
+  port: 5432,
+  logging: false, // Disable logging
+  dialectOptions: {
+    ssl: {
+      require: true, // Enforce SSL
+      rejectUnauthorized: false, // This will bypass certificate validation
+    },
+  },
 });
+
 
 // Function to connect to the database
 const connectDB = async () => {
@@ -15,8 +24,10 @@ const connectDB = async () => {
     console.log('PostgreSQL connected successfully');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
-    process.exit(1);
+     // Exit the process if unable to connect
   }
 };
 
+// Export the sequelize instance and connectDB function
 module.exports = { sequelize, connectDB };
+
